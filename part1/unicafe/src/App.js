243635c7560da2825ea727feb5ded857366b60e1
skempfer <shana.kempfer88@gmail.com>
2025-09-17 22:04:23 -0300

    Add exercise 1.10

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,39 +1,60 @@
-import { useState } from 'react'
-const Statistics = ({ good, neutral, bad }) => {
+import { useState } from 'react';
 
+
+const StaticLine = ({ text, content }) => {
   return (
-    <div>
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
-      <p>All: {good + neutral + bad}</p>
-      <p>Average: {(good - bad) / (good + neutral + bad)}</p>
-      <p>Positive: {good / (good + neutral + bad) * 100}%</p>
-    </div>
+    <p>{text}: {content}</p>
   )
-}
+};
+
+const Statistics = ({ good, neutral, bad }) => {
+  return (
+    <>
+      <StaticLine text="Good" content={good}/ >
+      <StaticLine text="Neutral" content={neutral}/ >
+      <StaticLine text="Bad" content={bad}/>
+      <StaticLine text="All" content={good + neutral + bad}/>
+      <StaticLine text="Average" content={(good - bad) / (good + neutral + bad)}/>
+      <StaticLine text="Positive" content={good / (good + neutral + bad) * 100}/>
+    </>
+  )
+};
+
+const Button = ({ onClick, text }) => {
+  return (
+    <button onClick={onClick}>{text}</button>
+  )
+};
 
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
- 
+  const handleGood = () => {
+    setGood(good + 1)
+  }
+  const handleNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+  const handleBad = () => {
+    setBad(bad + 1)
+  }
 
   return (
     <>
       <h1>Give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>Good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
-      <button onClick={() => setBad(bad + 1)}>Bad</button>
+      <Button text="Good" onClick={handleGood} />
+      <Button text="Neutral" onClick={handleNeutral} />
+      <Button text="Bad" onClick={handleBad} />
       <h1>Statistics</h1>
-      {good || neutral || bad !== 0 ? (
+      {good || neutral || bad > 0 ? (
         <Statistics good={good} neutral={neutral} bad={bad} />
       ) : (
-        <p>No feedback given</p>
+        <p>No feedback given.</p>
       )}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
