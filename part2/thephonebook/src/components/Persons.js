2e3e944951cf9ac51a9641f40461cfb82abea2d9
skempfer <shana.kempfer88@gmail.com>
2026-02-25 15:43:50 -0300

    Add exercises 2.11

--- part2/thephonebook/src/components/Persons.js
+++ part2/thephonebook/src/components/Persons.js
@@ -4,7 +4,7 @@ const Persons = ({ persons }) => {
     return (
         <>
             {persons.map(person => (
-                <Person key={person.name} person={person} />
+                <Person key={person.id} person={person} />
             ))}
         </>
     )
