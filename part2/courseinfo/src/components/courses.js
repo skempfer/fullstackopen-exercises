94a9ac60ae62be014e9ab081e99cfc51782ab947
skempfer <shana.kempfer88@gmail.com>
2026-02-25 14:10:20 -0300

    Add exercise 2.1

---
+++ part2/courseinfo/src/components/courses.js
@@ -0,0 +1,12 @@
+
+const Course = ({ course }) => {
+  return (
+    <>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </>
+  )
+}
+
+export default Course;
